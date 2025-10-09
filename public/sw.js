5f51a9b89a3d1225e970bdbb3edd5fb0ad375ee4
Vivek Kaushik <vivekmanepalli08@gmail.com>
2025-10-10 00:19:56 +0530

    new

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("droppoint-v1").then((cache) =>
+      cache.addAll(["/", "/manifest.json", "/icons/icon-192.png", "/icons/icon-512.png"])
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
